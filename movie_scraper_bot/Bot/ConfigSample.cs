786dbe824ffdaa5c91da78c36d4229ec23694fe8
alessandro <martinellialessandro04@gmail.com>
2022-01-22 17:17:05 +0100

    Removed keys

---
+++ movie_scraper_bot/Bot/ConfigSample.cs
@@ -0,0 +1,8 @@
+namespace Wrapper.Bot;
+
+public abstract class ConfigSample
+{
+    public static readonly List<string> RapidapiKeys = new();
+
+    public const string BotToken = "Telegram BotToken";
+}
